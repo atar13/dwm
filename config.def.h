61f3349601e7411b6bf02f5492f8fa7a345437e5
Anthony Tarbinian <atar137h@gmail.com>
2021-12-14 11:43:31 -0800

    Added custom keybinds and colors

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -10,19 +11,26 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = {"JetBrainsMono Nerd Font", "FiraCode Nerd Font", "MesloLGS NF", "Noto Color Emoji","NotoColorEmoji","Hack:size=10"};
+static const char dmenufont[]       = "MesloLGS NF:size=10";
+static const char col_gray1[]       = "#2D3132";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_purple[]      = "#9400D3";
+static const char col_black[] 	    = "#000000";
+static const char col_gray[]	    = "#545454";
+static const char col_brightpurple[] = "#7F00FF";
+static const char col_pastelpurple[] = "#A599E9";
+static const char col_clear[] 	    = "#a9a9a9";
+static const char col_pink[] 	    = "#F58FE9";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_black, col_gray2 },
+	[SchemeSel]  = { col_black, col_brightpurple,  col_brightpurple },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -57,7 +65,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define ALT Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -69,22 +78,57 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, "-h", "30", NULL };
+static const char *roficmd[] = {"rofi", "-no-lazy-grab", "-show", "drun", "-modi", "drun", "-theme", "~/.config/rofi/launchers/colorful/style_7.rasi", NULL };
+
+static const char *termcmd[]  = { "kitty", NULL };
+
+static const char *lockcmd[] = { "/home/atarbinian/.local/bin/lock", "&", NULL };
+static const char *firefoxcmd[] = {"firefox", NULL };
+static const char *firefoxprivatecmd[] = {"firefox", "--private-window", NULL };
+
+/*brightness comamnds */
+static const char *inc_brightness[] = {"light", "-A", "5", NULL};
+static const char *dec_brightness[] = {"light", "-U", "5", NULL};
+
+/*volume commands */
+static const char *toggle_Mute[] = {"pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
+static const char *inc_volume[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
+static const char *dec_volume[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
+
+static const char *play_cmd[] = {"playerctl", "play-pause", NULL};
+static const char *next_cmd[] = {"playerctl", "next", NULL};
+static const char *previous_cmd[] = {"playerctl", "previous", NULL};
+
+static const char *scrotcmd[] = {"sh", "/home/atarbinian/.scrot-select.sh", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY,                       XK_space,  spawn,      	   {.v = roficmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefoxcmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = firefoxprivatecmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
+	{ MODKEY|ShiftMask,             XK_v,      spawn,          {.v = scrotcmd } },
+	{ 0,                            XK_Print,      						 spawn,          {.v = scrotcmd } },
+	{ 0,             								XF86XK_MonBrightnessDown,  spawn,          {.v = dec_brightness } },
+	{ 0,           									XF86XK_MonBrightnessUp,    spawn,          {.v = inc_brightness } },
+	{ 0,             								XF86XK_AudioMute,     		 spawn,          {.v = toggle_Mute } },
+	{ 0,             								XF86XK_AudioLowerVolume,   spawn,          {.v = dec_volume } },
+	{ 0,             								XF86XK_AudioRaiseVolume,   spawn,          {.v = inc_volume } },
+	{ 0,             								XF86XK_AudioPlay,     		 spawn,          {.v = play_cmd } },
+	{ 0,             								XF86XK_AudioPrev,   			 spawn,          {.v = previous_cmd } },
+	{ 0,             								XF86XK_AudioNext,     		 spawn,          {.v = next_cmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_a,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_s,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ ALT,                       		XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -106,7 +150,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_grave,      quit,           {0} },
 };
 
 /* button definitions */
